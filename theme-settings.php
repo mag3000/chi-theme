ea0716ce2935124a97167672c99aeb59124f99e3
Michael Girgis <michaelg@uchicago.edu>
2011-06-17 17:37:05 -0500

    Moved scripts to bottom; removed contact form styles; added db backup with webform config; theme setting updates to include events calendar fields

--- theme-settings.php
+++ theme-settings.php
@@ -25,16 +25,46 @@ function uchicago_form_system_theme_settings_alter(&$form, &$form_state) {
 		'#markup' => '<table><tr><th>Phoenix:</th><th>Plumb Tree:</th></tr><tr><td><img src="/' . drupal_get_path('theme', 'uchicago') . '/images/screenshots/phoenix.png" alt="Phoenix theme" /></td><td><img src="/' . drupal_get_path('theme', 'uchicago') . '/images/screenshots/plumbtree.png" alt="Plumb Tree theme" /></td></tr><tr><th>Reflection Pool:</th><th>Autumn Leaves:</th></tr><tr><td><img src="/' . drupal_get_path('theme', 'uchicago') . '/images/screenshots/reflectionpool.png" alt="Reflection Pool theme" /></td><td><img src="/' . drupal_get_path('theme', 'uchicago') . '/images/screenshots/autumnleaves.png" alt="Autumn Leaves theme" /></td></tr></table>',
 		'#weight' => -9,
 	);
+
+	$form['calendar_api_key'] = array(
+		'#type' => 'textfield',
+		'#title' => t('Events calendar API key'),
+		'#default_value' => theme_get_setting('calendar_api_key'),
+		'#size' => 50,
+		'#maxlength' => 128,
+		'#required' => FALSE,
+		'#weight' => -9,
+	);
 	
 	$form['calendar_id'] = array(
 		'#type' => 'textfield',
-		'#title' => t('Events calendar ID'),
+		'#title' => t('Events calendar widget ID'),
 		'#default_value' => theme_get_setting('calendar_id'),
 		'#size' => 10,
 		'#maxlength' => 128,
 		'#required' => FALSE,
 		'#weight' => -8,
 	);	
+	
+	$form['calendar_list_id'] = array(
+		'#type' => 'textfield',
+		'#title' => t('Events list widget ID'),
+		'#default_value' => theme_get_setting('calendar_list_id'),
+		'#size' => 10,
+		'#maxlength' => 128,
+		'#required' => FALSE,
+		'#weight' => -7,
+	);	
+	
+	$form['calendar_rss_id'] = array(
+		'#type' => 'textfield',
+		'#title' => t('Events RSS widget ID'),
+		'#default_value' => theme_get_setting('calendar_rss_id'),
+		'#size' => 10,
+		'#maxlength' => 128,
+		'#required' => FALSE,
+		'#weight' => -6,
+	);		
 	
 	$form['theme_settings'] = array(
 		'#disabled' => TRUE,
